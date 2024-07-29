1895c7536878b289bd12db6388735d3017364ad5
Samuel-Anjoh-Jr <anjohsamueljr@gmail.com>
2024-07-29 19:42:27 +0100

    Added hashing

--- src/controller/auth_controller.js
+++ src/controller/auth_controller.js
@@ -1,19 +1,21 @@
 const dbconnection = require('../database');
+const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res) => {
     try {
         let {name, email, password} = req.body;
-    await dbconnection.query(
-        'INSERT INTO user(name,email,password) VALUES (?,?,?)', [name, email, password]
-    );
-    const user_inserted = await dbconnection.query(
-        'SELECT * FROM user WHERE id IN(SELECT MAX(id) FROM user)'
-    );
-    res.status(201).send({
-        success: true,
-        data: user_inserted[0],
-        message: 'Saved Successfully'
-    })
+        let hashPassword = await bcrypt.hash(password, 10);
+        await dbconnection.query(
+            'INSERT INTO user(name,email,password) VALUES (?,?,?)', [name, email, hashPassword]
+        );
+        const user_inserted = await dbconnection.query(
+            'SELECT * FROM user WHERE id IN(SELECT MAX(id) FROM user)'
+        );
+        res.status(201).send({
+            success: true,
+            data: user_inserted[0],
+            message: 'Saved Successfully'
+        })
     } catch (error) {
         res.status(500).send({
             success: false,
@@ -26,27 +28,65 @@ exports.signup = async (req, res) => {
 exports.signin = async (req, res) => {
     try {
         let {email, password} = req.body;
-    const user_found = await dbconnection.query(
-        'SELECT * FROM user WHERE email = ? AND password = ?', [email, password]
-    );
-    if(user_found[0].length===0){
-        res.status(401).send({
-            success: false,
-            data: [],
-            message: "Wrong email or password"
-        })
-    }else{
-        res.status(201).send({
-            success: true,
-            data: user_found[0],
-            message: 'Saved Successfully'
+        const user_found = await dbconnection.query(
+            'SELECT * FROM user WHERE email = ?', [email]
+        );
+        let user = user_found[0];
+        let verifyPassword = await bcrypt.compare(password, user[0].password, (error, result) => {
+            console.log(result)
         });
-    }
+        console.log(verifyPassword);
+        if(user_found[0].length===0){
+            res.status(401).send({
+                success: false,
+                data: [],
+                message: "Wrong email"
+            })
+        }else if(verifyPassword && user_found[0].length!=0){
+                res.status(201).send({
+                    success: true,
+                    data: user_found[0],
+                    message: 'Saved Successfully'
+                });
+        }else{
+                res.status(401).send({
+                    success: false,
+                    data: [],
+                    message: 'Wrong Password'
+                });
+            }
     } catch (error) {
         res.status(500).send({
             success: false,
             data: [],
             message: "Error on server: "+error.stack
         })
+    }
+}
+
+exports.updateUser = async (req, res) =>{
+    try {
+        let {name, email, password} = req.body;
+        let hashPassword = await bcrypt.hash(password, 10);
+        let id = req.query.id;
+        await dbconnection.query(
+            "UPDATE user SET name = ?, email = ?, password = ? WHERE id= ?",
+             [name, email, hashPassword, id]
+            );
+        const updateStudent = await dbconnection.query(
+            "SELECT * FROM user WHERE id = ?",
+            [id]
+        );
+        res.status(201).send({
+            success: true,
+            data: updateStudent[0],
+            message: 'Successfully Updated'
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            data: [],
+            message: error.stack
+        });
     }
 }
--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -4,5 +4,6 @@ const auth_controller = require('../controller/auth_controller');
 
 route.post('/signup', auth_controller.signup);
 route.post('/signin', auth_controller.signin);
+route.put('/', auth_controller.updateUser);
 
 module.exports = route
