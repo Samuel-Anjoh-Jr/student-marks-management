ad186f0df6afb64d1c09fb4d8b7f742b02bf767e
Samuel-Anjoh-Jr <anjohsamueljr@gmail.com>
2024-08-20 13:54:49 +0100

    Added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const bodyparser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const stud_route = require('./src/routes/student_route');
 const subj_route = require('./src/routes/subject_route');
 const ma_route = require('./src/routes/mark_route');
 const auth_route = require('./src/routes/auth_route');
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(bodyparser.json());
 
 app.use('/student', stud_route);
